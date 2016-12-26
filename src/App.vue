182d73c0e429f3623cb1acf246c685b0f7657af1
PolanZ <523608375@qq.com>
2016-12-26 17:39:10 +0800

    add

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <header class="app-header">
+      <app-header></app-header>
+    </header>
+  </div>
+</template>
+
+<script>
+import './assets/css/reset.css'
+import './assets/css/common.css'
+import Header from './components/Header.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'app-header': Header
+  }
+}
+</script>
+
+<style >
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+	<transition name="slide">
+		<div class="cover_top">
+			
+		</div>
+	</transition>
+</template>
+
+<style scoped>
+
+</style>
